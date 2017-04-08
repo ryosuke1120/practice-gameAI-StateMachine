205675907e8548e4bfe0a80c41b4d0ddc61224c1
Ryosuke Nakamura <nakaryou1120@gmail.com>
2017-04-08 16:59:21 +0900

    add agent function

---
+++ agent/misc/sample_inputkey.py
@@ -0,0 +1,14 @@
+from getch import getch
+ 
+print ("Push any key. (ESC : exit)")
+ 
+while True:
+    key = ord(getch())
+    print(key)
+    if key == 27: # ESC
+        print ("ESC : exit.")
+        break
+    elif key == 119: # W key
+        print ("Down W Key")
+    elif key == 113: # Q key
+        print ("Down Q Key")
