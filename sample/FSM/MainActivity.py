b6cca289446b3086839b90658b8e4114c22874dc
Ryosuke Nakamura <nakaryou1120@gmail.com>
2017-04-10 17:16:35 +0900

    add sample FSM

---
+++ sample/FSM/MainActivity.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+from models.Fox import *
+
+
+# class MainActivity : AppCompatActivity() 
+class MainActivity(object): 
+
+	def __init__(self):
+		self.myFox = Fox(1, 10)
+ 
+	def run_fox(self): 
+		self.myFox.update()
+		# fox_message.text = myFox.getMessage()
+		print(self.myFox.getMessage())
+
+if __name__ == "__main__":
+	a_game = MainActivity()
+	while True:
+		a_game.run_fox()
+		time.sleep(1)
